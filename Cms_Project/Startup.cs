ad3c2ab1f2aac4b0ca1d08ba74039d26e1590a85
Lenovo <Lenovo@LAPTOP-FU9P337P>
2024-04-29 00:47:27 +0330

    Add project files.

---
+++ Cms_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cms_Project.Startup))]
+namespace Cms_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
